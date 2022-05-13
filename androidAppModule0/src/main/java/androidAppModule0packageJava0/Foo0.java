32448ebf38d27337024145a91a7273acbc12253f
Łukasz Jarka <lukasz.jarka@gmail.com>
2022-05-13 13:23:56 +0200

    Initial commit

---
+++ androidAppModule0/src/main/java/androidAppModule0packageJava0/Foo0.java
@@ -0,0 +1,124 @@
+package androidAppModule0packageJava0;
+
+public class Foo0 {
+  public void foo0() {
+    new module85packageKt0.Foo0().foo4();
+    new module56packageKt0.Foo0().foo4();
+    new module45packageKt0.Foo0().foo4();
+    new module65packageKt0.Foo0().foo4();
+    new module17packageKt0.Foo0().foo4();
+    new module67packageKt0.Foo0().foo4();
+    new module88packageKt0.Foo0().foo4();
+    new module21packageKt0.Foo0().foo4();
+    new module66packageKt0.Foo0().foo4();
+    new module79packageKt0.Foo0().foo4();
+    new module72packageKt0.Foo0().foo4();
+    new module75packageKt0.Foo0().foo4();
+    new module16packageKt0.Foo0().foo4();
+    new module2packageKt0.Foo0().foo4();
+    new module33packageKt0.Foo0().foo4();
+    new module47packageKt0.Foo0().foo4();
+    new module82packageKt0.Foo0().foo4();
+    new module60packageKt0.Foo0().foo4();
+    new module49packageKt0.Foo0().foo4();
+    new module92packageKt0.Foo0().foo4();
+    new module23packageKt0.Foo0().foo4();
+    new module96packageKt0.Foo0().foo4();
+    new module69packageKt0.Foo0().foo4();
+    new module94packageKt0.Foo0().foo4();
+    new module100packageKt0.Foo0().foo4();
+    new module64packageKt0.Foo0().foo4();
+    new module40packageKt0.Foo0().foo4();
+    new module98packageKt0.Foo0().foo4();
+    new module91packageKt0.Foo0().foo4();
+    new module32packageKt0.Foo0().foo4();
+    new module62packageKt0.Foo0().foo4();
+    new module42packageKt0.Foo0().foo4();
+    new module24packageKt0.Foo0().foo4();
+    new module13packageKt0.Foo0().foo4();
+    new module15packageKt0.Foo0().foo4();
+    new module36packageKt0.Foo0().foo4();
+    new module57packageKt0.Foo0().foo4();
+    new module28packageKt0.Foo0().foo4();
+    new module25packageKt0.Foo0().foo4();
+    new module30packageKt0.Foo0().foo4();
+    new module71packageKt0.Foo0().foo4();
+    new module20packageKt0.Foo0().foo4();
+    new module93packageKt0.Foo0().foo4();
+    new module29packageKt0.Foo0().foo4();
+    new module8packageKt0.Foo0().foo4();
+    new module39packageKt0.Foo0().foo4();
+    new module87packageKt0.Foo0().foo4();
+    new androidAppModule1packageKt0.Foo0().foo4();
+    new module53packageKt0.Foo0().foo4();
+    new module76packageKt0.Foo0().foo4();
+    new module70packageKt0.Foo0().foo4();
+    new module90packageKt0.Foo0().foo4();
+    new module77packageKt0.Foo0().foo4();
+    new module46packageKt0.Foo0().foo4();
+    new module99packageKt0.Foo0().foo4();
+    new module5packageKt0.Foo0().foo4();
+    new module95packageKt0.Foo0().foo4();
+    new module55packageKt0.Foo0().foo4();
+    new module54packageKt0.Foo0().foo4();
+    new module58packageKt0.Foo0().foo4();
+    new module14packageKt0.Foo0().foo4();
+    new module4packageKt0.Foo0().foo4();
+    new module9packageKt0.Foo0().foo4();
+    new module97packageKt0.Foo0().foo4();
+    new module43packageKt0.Foo0().foo4();
+    new module10packageKt0.Foo0().foo4();
+    new module84packageKt0.Foo0().foo4();
+    new module63packageKt0.Foo0().foo4();
+    new module1packageKt0.Foo0().foo4();
+    new module78packageKt0.Foo0().foo4();
+    new module35packageKt0.Foo0().foo4();
+    new module26packageKt0.Foo0().foo4();
+    new module81packageKt0.Foo0().foo4();
+    new module51packageKt0.Foo0().foo4();
+    new module41packageKt0.Foo0().foo4();
+    new module3packageKt0.Foo0().foo4();
+    new module74packageKt0.Foo0().foo4();
+    new module68packageKt0.Foo0().foo4();
+    new module73packageKt0.Foo0().foo4();
+    new module89packageKt0.Foo0().foo4();
+    new module34packageKt0.Foo0().foo4();
+    new module22packageKt0.Foo0().foo4();
+    new module61packageKt0.Foo0().foo4();
+    new module83packageKt0.Foo0().foo4();
+    new module7packageKt0.Foo0().foo4();
+    new module12packageKt0.Foo0().foo4();
+    new module18packageKt0.Foo0().foo4();
+    new module0packageKt0.Foo0().foo4();
+    new module31packageKt0.Foo0().foo4();
+    new module11packageKt0.Foo0().foo4();
+    new module6packageKt0.Foo0().foo4();
+    new module38packageKt0.Foo0().foo4();
+    new module44packageKt0.Foo0().foo4();
+    new module37packageKt0.Foo0().foo4();
+    new module27packageKt0.Foo0().foo4();
+    new module19packageKt0.Foo0().foo4();
+    new module59packageKt0.Foo0().foo4();
+    new module48packageKt0.Foo0().foo4();
+    new module80packageKt0.Foo0().foo4();
+    new module52packageKt0.Foo0().foo4();
+    new module86packageKt0.Foo0().foo4();
+    new module50packageKt0.Foo0().foo4();
+  }
+
+  public void foo1() {
+    foo0();
+  }
+
+  public void foo2() {
+    foo1();
+  }
+
+  public void foo3() {
+    foo2();
+  }
+
+  public void foo4() {
+    foo3();
+  }
+}
